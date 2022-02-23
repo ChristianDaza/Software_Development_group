982766d9ff769c40b5b9a31f0baa483decb7421f
yaseminbridges <93208398+yaseminbridges@users.noreply.github.com>
2022-02-23 13:23:23 +0000

    add pkl functions

--- Stats/statistics.py
+++ Stats/statistics.py
@@ -39,6 +39,28 @@ def nucleotide_diversity_list(filelist, mini, maxi):
     results = df.DataFrame.from_dict(results, orient='index', columns=['Nucleotide Diversity'])  # return all results as a dataframe
     return results
   
+    
+    
+###### Nucleotide Diversity (accepts a list of pkl) ######
+
+def nucleotide_diversity_pkl(filelist, mini, maxi):
+    """ Computes nucleotide diversity within a given region from a list of CSV files containing position and haplotype data. """
+    results = {}
+    for file in filelist:
+        df = file
+        new_dataframe = df.filter(['POS'])  # extract the position from the dataframe
+        position = pd.DataFrame(new_dataframe, columns=['POS']).to_numpy()
+        position = position.flatten()    # alter position to one dimensional numpy array for correct input for sequence diversity function
+        df = df.drop(['POS'], axis=1)
+        country = df.columns[0]    # extract the population code
+        arr = pd.DataFrame(df).to_numpy()     # haplotype dataframe to numpy array
+        haplotypes = allel.HaplotypeArray(arr)   
+        geno = haplotypes.to_genotypes(ploidy=2)   # reshape haplotype array to genotype array for subsequent correct function input
+        ac = geno.count_alleles()
+        pi = allel.sequence_diversity(position, ac, start=mini, stop=maxi) # nucleotide diversity calculation
+        results[country] = pi
+    results = df.DataFrame.from_dict(results, orient='index', columns=['Nucleotide Diversity'])  # return all results as a dataframe
+    return results
   
 ###### Haplotype Diversity ######
 
@@ -97,6 +119,28 @@ def tajima_D_list(filelist, mini, maxi):
     results = pd.DataFrame.from_dict(results, orient='index', columns=['Tajima D'])  # return all results as a dataframe
     return results
 
+###### Tajima's D (accepts a list of pkl)######
+
+def tajima_D_list(filelist, mini, maxi):
+    """ Computes Tajima's D within a given region from list of CSV files containing position and haplotype data. """
+    results = {}
+    for file in filelist:
+        df = file
+        df = df.loc[(df['POS'] >= mini) & (df['POS'] <= maxi)]
+        new_dataframe = df.filter(['POS'])  # extract the position from the dataframe
+        position = pd.DataFrame(new_dataframe, columns=['POS']).to_numpy()
+        position = position.flatten()    # alter position to one dimensional numpy array for correct input for sequence diversity function
+        df = df.drop(['POS'], axis=1)
+        country = df.columns[0]    # extract the population code
+        arr = pd.DataFrame(df).to_numpy()     # haplotype dataframe to numpy array
+        haplotypes = allel.HaplotypeArray(arr)   
+        geno = haplotypes.to_genotypes(ploidy=2)   # reshape haplotype array to genotype array for subsequent correct function input
+        ac = geno.count_alleles()
+        t = allel.tajima_d(ac, pos=position, start=mini, stop=maxi)  # Tajima's D calculation
+        results[country] = t
+    results = pd.DataFrame.from_dict(results, orient='index', columns=['Tajima D'])  # return all results as a dataframe
+    return results
+
 ###### Homozygosity (accepts a single file) ######
 
 def homozygosity(inputfile, mini, maxi):
@@ -126,6 +170,34 @@ def homozygosity_2(filelist, mini, maxi):
     results = {}
     for file in filelist:
         df = pd.read_csv(file, index_col=0)
+        df= df.loc[(df['POS'] >= mini) & (df['POS'] <= maxi)]
+        new_dataframe = df.filter(['POS'])  # extract the position from the dataframe
+        position = pd.DataFrame(new_dataframe, columns=['POS']).to_numpy()
+        position = position.flatten()    # alter position to one dimensional numpy array for correct input for sequence diversity function
+        df = df.drop(['POS'], axis=1)
+        country = df.columns[0]
+        num = df.shape[1] / 2
+        arr = pd.DataFrame(df).to_numpy()
+        haplotypes = allel.HaplotypeArray(arr)
+        gt = haplotypes.to_genotypes(ploidy=2)
+        h = gt.is_hom()
+        homo = np.sum(h, axis=1)
+        homo= pd.DataFrame(homo, columns=['HOMOZYGOSITY'])
+        homo['HOMOZYGOSITY'] = homo['HOMOZYGOSITY'].div(num)
+        homozygosity =  homo['HOMOZYGOSITY'].sum() / len(homo.index)
+        results[country] = homozygosity
+    results = pd.DataFrame.from_dict(results, orient='index', columns=['Homozygosity'])
+    return results
+
+
+###### Homozygosity (accepts a list of files) ######
+
+
+def homozygosity_pkl(filelist, mini, maxi):
+    """ Takes a list of CSV files as its input and returns the average homozygosity over a given region. """
+    results = {}
+    for file in filelist:
+        df = file
         df= df.loc[(df['POS'] >= mini) & (df['POS'] <= maxi)]
         new_dataframe = df.filter(['POS'])  # extract the position from the dataframe
         position = pd.DataFrame(new_dataframe, columns=['POS']).to_numpy()
